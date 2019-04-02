089adc1971619bc94f08dcb545a5cd9e69803b26
jafiecht <jafiecht@purdue.edu>
2019-04-02 16:29:37 -0400

    Climatic csv file created: no normalization

--- compiler.py
+++ compiler.py
@@ -4,7 +4,20 @@ import os
 #get the filenames of the raw dataset
 filenames = os.listdir('raw_data')
 
+#Years of interest
 years = [1991, 1996, 2001, 2006, 2011]
+
+#Names of the data to be summarized annually
+annual_names = ['year', 'janprcp', 'febprcp', 'marprcp', 'aprprcp', 'mayprcp', 'junprcp', 'julprcp', 'augprcp', 'sepprcp', 'octprcp', 'novprcp', 'decprcp', 'totprcp', 'jancold', 'febcold', 'marcold', 'aprcold', 'maycold', 'juncold', 'julcold', 'augcold', 'sepcold', 'octcold', 'novcold', 'deccold', 'totcold', 'janheat', 'febheat', 'marheat', 'aprheat', 'mayheat', 'junheat', 'julheat', 'augheat', 'sepheat', 'octheat', 'novheat', 'decheat', 'totheat']
+
+##################################################
+#Create a dataframe to write to
+col_names = ['year', 'state'] + annual_names[1:]
+for i in range(1,6):
+  for annual_name in annual_names[1:]:
+    col_names.append(annual_name + '_' + str(i))
+
+master = pd.DataFrame(columns = col_names)
 
 ##################################################
 #Extract values
@@ -44,21 +57,40 @@ def yearly(year):
 
 ##################################################
 #Repeat the process for each filename
-#for filename in filenames:
-for file in range(1):
-  raw = pd.read_csv('raw_data/'+filenames[0])
+overall_index = 0
+for filename in filenames:
+  state = os.path.splitext(filename)[0]
+  print(state)
+  
+  raw = pd.read_csv('raw_data/'+filename)
   raw['DATE'] = pd.to_datetime(raw['DATE'], format='%Y-%m-%d')
 
-  col_names = ['year', 'janprcp', 'febprcp', 'marprcp', 'aprprcp', 'mayprcp', 'junprcp', 'julprcp', 'augprcp', 'sepprcp', 'octprcp', 'novprcp', 'decprcp', 'totprcp', 'jancold', 'febcold', 'marcold', 'aprcold', 'maycold', 'juncold', 'julcold', 'augcold', 'sepcold', 'octcold', 'novcold', 'deccold', 'totcold', 'janheat', 'febheat', 'marheat', 'aprheat', 'mayheat', 'junheat', 'julheat', 'augheat', 'sepheat', 'octheat', 'novheat', 'decheat', 'totheat']
-  annual = pd.DataFrame(columns = col_names)
+  annual = pd.DataFrame(columns = annual_names)
   index = 0
-  for i in range(1960, 2012): 
+  for i in range(1960, 2012):
     summary = yearly(raw.loc[(raw['DATE'].dt.year==i)])
-    row  = [int(i)] + summary
+    row  = [i] + summary
     annual.loc[index] = row
     index = index + 1
-  print(annual['year'][0])
-  print(type(annual['year'][0]))
-  #print(annual.loc[(annual['year'] > 1959 & annual['year'] < 1961)])
+  
+  #If it's needed to normalize, this will be how it's done
+  #historical = annual.loc[(annual['year'] > 1959) & (annual['year'] < 1991)].copy()
+  #for col_name in col_names[1:]:
+    #average = historical[col_name].mean()
+    #annual[col_name] = annual[col_name] / average
+ 
+  for year in years:
+    row = [year, state] 
+    primary =  annual.loc[(annual['year'] == year)].values.tolist()[0][1:]
+    lag1 = annual.loc[(annual['year'] == (year-1))].values.tolist()[0][1:]
+    lag2 = annual.loc[(annual['year'] == (year-2))].values.tolist()[0][1:]
+    lag3 = annual.loc[(annual['year'] == (year-3))].values.tolist()[0][1:]
+    lag4 = annual.loc[(annual['year'] == (year-4))].values.tolist()[0][1:]
+    lag5 = annual.loc[(annual['year'] == (year-5))].values.tolist()[0][1:]
+    row = row + primary + lag1 + lag2 + lag3 + lag4 + lag5
 
+    master.loc[overall_index] = row
+    overall_index = overall_index + 1
 
+master.to_csv('climatic_data.csv', index=False)
+print(master)
